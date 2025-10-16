655d0861ab2ba09eac00e38d49c3d135944ddd33
manasishinde681 <manasishinde681@gmail.com>
2025-10-16 21:56:21 +0530

    Add files via upload

---
+++ bubbleSorting.java
@@ -0,0 +1,27 @@
+public class practicebubble{
+
+    public static void bubbleSort(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    //swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int arr[]){
+            for(int i=0; i<arr.length; i++){
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+        }
+
+    public static void main(String args[]){
+        int arr[]={2,4,6,10,8,9,1};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
